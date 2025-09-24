caee239a35973d5b056923725ebb75264083f0ea
L0703395 <locasciokaylie@gmail.com>
2025-09-23 20:23:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: Set base to your repo name so assets resolve on GitHub Pages.
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/blog/"
-});
+  base: command === "build" ? "/blog/" : "/",
+}));
