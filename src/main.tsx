786e614091734097f38a7768a799dc120e04489d
L0703395 <locasciokaylie@gmail.com>
2025-09-23 20:40:26 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-console.log("Mounting React app…"); // diagnostic – safe to keep
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
